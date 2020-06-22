47895cc5b8114e63451d76b8a398cd6e93a95090
elensiya <valya.okulova@mail.ru>
2020-06-22 21:14:57 +0700

    changed nullable fields to none nullable

--- mainpage/models.py
+++ mainpage/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from myauth.models import User
 
 class Deal(models.Model):
-    title = models.CharField(max_length = 40, blank=True)
-    more = models.CharField(max_length = 200, blank=False, default="Отсутствует")
-    image = models.CharField(max_length = 150, blank=False, default="https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png")
-    address = models.CharField(max_length = 100, blank=False, default="Отсутствует")
-    price = models.CharField(max_length=40, blank=False, default="Отсутствует")
-    by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length = 40, blank=False)
+    more = models.CharField(max_length = 200, blank=True, default="Отсутствует")
+    image = models.CharField(max_length = 150, blank=True, default="https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png")
+    address = models.CharField(max_length = 100, blank=True, default="Отсутствует")
+    price = models.CharField(max_length=40, blank=True, default="Отсутствует")
+    by = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
     
 
     
