3f24fa6a7e6e939cb21adc5e0f26763eb0f6f30d
elensiya <valya.okulova@mail.ru>
2020-06-23 12:18:08 +0700

    trying to upload images. Need to change a

--- mainpage/models.py
+++ mainpage/models.py
@@ -4,10 +4,6 @@ from myauth.models import User
 class Deal(models.Model):
     title = models.CharField(max_length = 40, blank=False)
     more = models.CharField(max_length = 200, blank=True, default="Отсутствует")
-    image = models.CharField(max_length = 150, blank=True, default="https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png")
+    image = models.ImageField(blank=True, upload_to = "images/", default="https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png")
     address = models.CharField(max_length = 100, blank=True, default="Отсутствует")
     price = models.CharField(max_length=40, blank=True, default="Отсутствует")
-    by = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
-    
-
-    
--- mainpage/views.py
+++ mainpage/views.py
@@ -5,6 +5,15 @@ from myauth.models import User
 from django.contrib import messages
 from django.shortcuts import redirect
 
+
+def handle_uploaded_file(f):
+    destination = open('addfile', 'wb+')
+    for chunk in f.chunks():
+        destination.write(chunk)
+    destination.close()
+
+
+
 def main(request):
     items = list(Deal.objects.all())
     user = User.objects.get(login=request.session['username'])
@@ -17,12 +26,16 @@ def main(request):
 
 def add(request):
     if request.method == 'POST':
-        form = create(request.POST)
+        form = create(request.POST, request.FILES)
         if form.is_valid():
-            newDeal= User.objects.get(login=request.session['username'])
+            print(request.FILES)
+            #handle_uploaded_file(request.FILES.get('image'))
             form.save()
-            return redirect('/main')
-    
+            newDeal= list(Deal.objects.all())[-1]
+            return redirect('/main/detail/'+str(newDeal.id))
+    else:
+        form = create()
+
     user = User.objects.get(login=request.session['username']) 
     context = {
         "image": user.image,
